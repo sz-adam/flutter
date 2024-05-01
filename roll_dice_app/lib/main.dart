4fbea84a80f73a64150e6c28ea2a2891ee8c581f
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-01 11:20:47 +0200

    linear gradient

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello world'),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent, Colors.blueGrey],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello world'),
+          ),
         ),
       ),
     ),
