b55215d072e2ae1bf36e601373a7386d100ebc2e
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-01 15:17:36 +0200

    color outsourcing

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2 , {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-final Color color1 ;
-final Color color2 ;
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1,color2],
+          colors: [color1, color2],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: const Center(
-        child: StyledText('Hello word',68),
+        child: StyledText('Hello word', 28, Color.fromARGB(255, 255, 255, 255)),
       ),
     );
   }
--- roll_dice_app/lib/styled_text.dart
+++ roll_dice_app/lib/styled_text.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText(this.text, this.fontSize, {super.key});
+  const StyledText(this.text, this.fontSize, this.color, {super.key});
 
   final String text;
-  final double fontSize; // Change type to double
+  final double fontSize; 
+  final Color color;
 
   @override
   Widget build(context) {
     return Text(
       text,
-      style: TextStyle(color: Colors.white, fontSize: fontSize),
+      style: TextStyle(color: color, fontSize: fontSize),
     );
   }
 }
