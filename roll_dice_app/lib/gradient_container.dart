18e4ed7a08bac085474daf076102d43b78d90333
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-01 15:12:41 +0200

    gradient color outsourcing

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2 , {super.key});
+
+final Color color1 ;
+final Color color2 ;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Colors.blueAccent,
-            Colors.blueGrey,
-          ],
+          colors: [color1,color2],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('Hello word',68),
       ),
     );
   }
