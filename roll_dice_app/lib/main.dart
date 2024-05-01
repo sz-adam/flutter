fd8ea85f4fc7fbeb4c614dd3032518175212ae42
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-01 11:55:32 +0200

    with Constructor Functions

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
@@ -10,28 +10,28 @@ void main() {
   );
 }
 
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
 
-class GradientContainer extends StatelessWidget{
   @override
- Widget build(context){
-  return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.blueAccent,
-                Colors.blueGrey,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        )
-;
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.blueAccent,
+            Colors.blueGrey,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
   }
 }
