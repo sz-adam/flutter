60ed30b50561b32254f3b8c34aceae2e47f1f7b5
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-12 10:39:41 +0200

    start screen image display, text and button

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: StartScreen(),
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          ),
+          child: const StartScreen()),
     ),
   ));
 }
