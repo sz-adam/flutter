be9d5a7949fdfbd59c93049945115280e1a19d99
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-12 16:57:12 +0200

    create skeleton

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -11,6 +11,38 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text("QuestionsScreens");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question ... ',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(),
+            child: const Text('Answer1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+             style: ElevatedButton.styleFrom(),
+            child: const Text('Answer2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+             style: ElevatedButton.styleFrom(),
+            child: const Text('Answer3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+             style: ElevatedButton.styleFrom(),
+            child: const Text('Answer4'),
+          )
+        ],
+      ),
+    );
   }
 }
