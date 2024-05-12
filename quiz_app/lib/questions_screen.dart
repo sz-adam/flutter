97cb81f0da2dc4a68eae51683b05c6ca1632cc11
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-12 18:38:25 +0200

    add buttons screen wide, padding Text-center

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -17,21 +17,26 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style:const  TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((item) {
-            
-            return  AnswerButton(answerText: item, onTap: () {} );
-          })
-      
+      child: Container(
+        margin:const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style:const  TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((item) {
+              
+              return  AnswerButton(answerText: item, onTap: () {} );
+            })
         
-        ],
+          
+          ],
+        ),
       ),
     );
   }
