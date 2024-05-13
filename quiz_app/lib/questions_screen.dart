d22f63d4213fa227a53b3fd5fee0d1efa8f20ec6
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-13 10:38:53 +0200

    google_fonts

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -36,7 +37,11 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style:const  TextStyle(color: Colors.white),
+              style:GoogleFonts.aboreto(
+                color:Colors.white,
+                fontSize:20,
+                fontWeight: FontWeight.bold
+              ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
