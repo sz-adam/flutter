28be8955d182bc3047cb0f6de7ad8073e02eb230
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-16 10:37:43 +0200

    display correct incorrect answers

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -43,7 +43,7 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        selectedAnswers = [];
+      
         activeScreen = 'result-screen';
       });
     }
--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,48 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/question_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
 
   final List<String> chosenAnswers;
 
   List<Map<String, Object>> getSummaryData() {
-    final List<Map<String, Object>> summary =[];
+    final List<Map<String, Object>> summary = [];
 
-for(var i=0; i<chosenAnswers.length; i++){
-  //loop body
-  summary.add({
-    'question_index': i,
-    'question':questions[i].text,
-    'correct_answer':questions[i].answers[0],
-    'user_answer':chosenAnswers[i],
-  });
-}
+    for (var i = 0; i < chosenAnswers.length; i++) {
+       //loop body
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
 
     return summary;
   }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('You answeredX out of Y question correctly'),
-            SizedBox(
+            const Text('You answered X out of Y questions correctly!'),
+            const SizedBox(
               height: 30,
             ),
-            Text('List of answers and question ...'),
-            SizedBox(
+            QuestionsSummary(getSummaryData()),
+            const SizedBox(
               height: 30,
             ),
-            //TextButton(
-            //  onPressed: () {},
-            //  child: Text('Restart quiz!'),
-            //),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Restart Quiz!'),
+            )
           ],
         ),
       ),
