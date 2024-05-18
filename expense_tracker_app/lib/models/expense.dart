63129436a35f52985c323b3a7dd8c3d771fdbfef
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-18 14:46:06 +0200

    add category model

--- expense_tracker_app/lib/models/expense.dart
+++ expense_tracker_app/lib/models/expense.dart
@@ -2,12 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, trave, leisure, work }
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.data})
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.data,
+      required this.category})
       : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime data;
+  final Category category;
 }
