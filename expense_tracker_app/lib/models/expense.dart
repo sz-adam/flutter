aa39ce43fb10a91ec800873dc4fdeac8a6164487
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-18 14:41:21 +0200

    create model

---
+++ expense_tracker_app/lib/models/expense.dart
@@ -0,0 +1,13 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Expense {
+  Expense({required this.title, required this.amount, required this.data})
+      : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime data;
+}
