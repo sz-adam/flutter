01e2ceaedc3b2868901c589fecd96d9a40c4bc2d
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-18 14:52:27 +0200

    create dummy data

---
+++ expense_tracker_app/lib/expense.dart
@@ -0,0 +1,33 @@
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [Text('The chart'), Text('Expenses List')],
+      ),
+    );
+  }
+}
