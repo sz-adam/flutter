6cb0978606bfd4db709d2ee5746026f6fe1faaa6
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-19 19:46:12 +0200

    background theme

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:expense_tracker_app/widget/expense.dart';
 import 'package:flutter/material.dart';
 
+var kColorSheme =
+    ColorScheme.fromSeed(seedColor:const Color.fromARGB(255, 96, 59, 181));
+
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorSheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorSheme.onPrimaryContainer,
+          foregroundColor: kColorSheme.primaryContainer
+        )
+      ),
+      home: const Expenses(),
     ),
   );
 }
