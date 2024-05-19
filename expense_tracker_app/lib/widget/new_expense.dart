775a1f2897a2c9724f1c10e9e69862bbec0f40de
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-19 13:35:28 +0200

    selected date is displayed

--- expense_tracker_app/lib/widget/new_expense.dart
+++ expense_tracker_app/lib/widget/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_app/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -17,6 +18,7 @@ class _NewExpenseState extends State<NewExpense> {
 
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
   //dispose a memoriából való törlést szolgálja TextEditingController-nél mindig szükséges
   @override
   void dispose() {
@@ -25,17 +27,24 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
- void _presentDatePicker()  {
-  final now = DateTime.now();
-  final firstDate = DateTime(now.year -1, now.month,now.day);
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
 
-    showDatePicker(context: context, initialDate: now ,firstDate: firstDate, lastDate: now);
+    final pickedData = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickedData;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
@@ -67,13 +76,17 @@ class _NewExpenseState extends State<NewExpense> {
               const SizedBox(
                 width: 16,
               ),
-               Expanded(
+              Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Selected Date'),
-                   IconButton(
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
                       onPressed: _presentDatePicker,
                       icon: const Icon(Icons.calendar_month),
                     ),
