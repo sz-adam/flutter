c90c42e900222c8940084af4bd34bab14255f256
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-19 10:12:20 +0200

    onchanged obtaining value

--- expense_tracker_app/lib/widget/new_expense.dart
+++ expense_tracker_app/lib/widget/new_expense.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 
-class NewExpense extends StatefulWidget{
-   const NewExpense({super.key});
- @override
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+  @override
   State<NewExpense> createState() {
-   return _NewExpenseState();
+    return _NewExpenseState();
   }
 }
 
+class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
 
-class _NewExpenseState extends State<NewExpense>{
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-   return const Padding(padding: EdgeInsets.all(16),
-   child: Column(
-    children: [TextField(
-      maxLength: 50,
-      //Billentyüzet fajta megnyitás
-      keyboardType: TextInputType.text,
-      decoration: InputDecoration(
-        label: Text('Title'),
-       
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            onChanged: _saveTitleInput,
+            maxLength: 50,
+            //Billentyüzet fajta megnyitás
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_enteredTitle);
+                  }, child: const Text('Save Expense'))
+            ],
+          )
+        ],
       ),
-    ),],
-   ),);
+    );
   }
 }
