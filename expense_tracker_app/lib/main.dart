d5897fd47ce735eaada26a496465868f47d7321e
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-19 19:57:49 +0200

    settings theme

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,19 +1,38 @@
 import 'package:expense_tracker_app/widget/expense.dart';
 import 'package:flutter/material.dart';
 
-var kColorSheme =
-    ColorScheme.fromSeed(seedColor:const Color.fromARGB(255, 96, 59, 181));
-
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 44, 76, 105),
+);
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(
-        colorScheme: kColorSheme,
+      theme: ThemeData().copyWith(       
+        colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorSheme.onPrimaryContainer,
-          foregroundColor: kColorSheme.primaryContainer
-        )
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
