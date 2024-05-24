0d0d516a4458a1af546b2921db109de1f7c32e3d
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-24 19:15:59 +0200

    display categories

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -9,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick your Category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(16),
         //grid elem :crossAxisCount 2 oszlop ,childAspectRatio: 3 / 2, // Szélesség / Magasság arány
         //rács elemek közötti távolság crossAxisSpacing: 20, mainAxisSpacing: 20
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -17,39 +20,11 @@ class CategoriesScreen extends StatelessWidget {
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
         children: [
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.amber),
-          ),
-          
+          //végig megyünk a availableCategories gyüjteményen és minden egyes elemhez létrehoz egy CategoryGridItem widgetet
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+
+            //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
         ],
       ),
     );
