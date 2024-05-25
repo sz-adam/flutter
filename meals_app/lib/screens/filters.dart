032eb4e0fe12a244dbc474199b2880ffbdd9badf
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-25 20:41:33 +0200

    Filterscreen navigation TabsScreen

---
+++ meals_app/lib/screens/filters.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/tabs.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Filters'),
+        ),
+        drawer: MainDrawer(
+          onSelectScreen: (identifier) {
+            Navigator.of(context).pop();
+            if (identifier == 'meals') {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const TabsScreen(),
+                ),
+              );
+            }
+          },
+        ),
+        body: Column(
+          children: [
+            //listában való megjelenítés
+            SwitchListTile(
+              value: _glutenFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilterSet = !isChecked;
+                });
+              },
+              title: Text(
+                "Gluten-free",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Only include gluten-free meals. ',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            )
+          ],
+        ));
+  }
+}
