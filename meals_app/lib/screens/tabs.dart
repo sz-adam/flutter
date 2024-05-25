cde1efaa92cfbf85dbfbb224915ddb3377b2499a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-25 16:31:23 +0200

    ScaffoldMessenger -> Notification add and remove

--- meals_app/lib/screens/tabs.dart
+++ meals_app/lib/screens/tabs.dart
@@ -14,14 +14,26 @@ class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
 
   final List<Meal> _favoriteMeals = [];
-  
+
+  void _showInfoMessage(String message){
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
     //ha szerepel a tömbben akkor töröljünk ha nem akkor hozzáadjuk
     if (isExisting) {
-      _favoriteMeals.remove(meal);    
+     setState(() {
+        _favoriteMeals.remove(meal);
+     });
+     _showInfoMessage('Meal is no longer a favorite');
     } else {
-      _favoriteMeals.add(meal);
+     setState(() {
+        _favoriteMeals.add(meal);
+        _showInfoMessage('Marked as a favorite');
+     });
+     
     }
   }
 
@@ -41,7 +53,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage =
-          MealsScreen(meals: [], onToggleFavorite: _toggleMealFavoriteStatus);
+          MealsScreen(meals: _favoriteMeals, onToggleFavorite: _toggleMealFavoriteStatus);
       activePageTitle = 'Your Favorites';
     }
 
