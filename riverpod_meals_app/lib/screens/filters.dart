d5cac7fddfb54fb4bd23b81f5cfebbfad81df1bd
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-29 09:35:53 +0200

    use the filterProvider

--- riverpod_meals_app/lib/screens/filters.dart
+++ riverpod_meals_app/lib/screens/filters.dart
@@ -1,36 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // import 'package:meals/screens/tabs.dart';
 // import 'package:meals/widgets/main_drawer.dart';
+import 'package:meals_app/providers/filter_provider.dart';
 
-enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
-
-class FiltersScreen extends StatefulWidget {
-  const FiltersScreen({super.key,required this.currentFilters});
-
-  final Map<Filter,bool> currentFilters;
+class FiltersScreen extends ConsumerStatefulWidget {
+  const FiltersScreen({super.key});
 
   @override
-  State<FiltersScreen> createState() {
+  ConsumerState<FiltersScreen> createState() {
     return _FiltersScreenState();
   }
 }
 
-class _FiltersScreenState extends State<FiltersScreen> {
+class _FiltersScreenState extends ConsumerState<FiltersScreen> {
   var _glutenFreeFilterSet = false;
   var _lactoseFreeFilterSet = false;
   var _vegetarianFilterSet = false;
   var _veganFilterSet = false;
 
-
-  //iniciliziáljuk az összes szűrőt , hogy ne álljon vissza alapból false-ra 
   @override
   void initState() {
     super.initState();
-    _glutenFreeFilterSet =widget.currentFilters[Filter.glutenFree]!;
-    _lactoseFreeFilterSet =widget.currentFilters[Filter.lactoseFree]!;
-    _vegetarianFilterSet =widget.currentFilters[Filter.vegetarian]!;
-    _veganFilterSet =widget.currentFilters[Filter.vegan]!;
+    final activeFilters = ref.read(filtersProvider);
+    _glutenFreeFilterSet = activeFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = activeFilters[Filter.lactoseFree]!;
+    _vegetarianFilterSet = activeFilters[Filter.vegetarian]!;
+    _veganFilterSet = activeFilters[Filter.vegan]!;
   }
 
   @override
@@ -55,13 +52,15 @@ class _FiltersScreenState extends State<FiltersScreen> {
         //vissza gomb viselkedését tiltja tabs.dart fájlban a navigáció megnyomásával visszaadja a kiválasztott filter értéket
         canPop: false,
         onPopInvoked: (bool didPop) {
-          if (didPop) return;
-          Navigator.of(context).pop({
+          ref.read(filtersProvider.notifier).setFilters({
             Filter.glutenFree: _glutenFreeFilterSet,
             Filter.lactoseFree: _lactoseFreeFilterSet,
             Filter.vegetarian: _vegetarianFilterSet,
             Filter.vegan: _veganFilterSet,
           });
+          if (didPop) return;
+        
+          Navigator.of(context).pop();
         },
         child: Column(
           children: [
--- riverpod_meals_app/lib/screens/tabs.dart
+++ riverpod_meals_app/lib/screens/tabs.dart
@@ -6,7 +6,7 @@ import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:meals_app/providers/filter_provider.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
@@ -15,18 +15,18 @@ const kInitialFilters = {
   Filter.vegan: false
 };
 
-//StatefulWidget átírása ConsumerStatefulWidget-re hogy a provider használható legyen 
+//StatefulWidget átírása ConsumerStatefulWidget-re hogy a provider használható legyen
 class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
-//StatefulWidget átírása ConsumerStatefulWidget-re hogy a provider használható legyen 
+
+//StatefulWidget átírása ConsumerStatefulWidget-re hogy a provider használható legyen
 class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
- 
-  Map<Filter, bool> _selectedFilters = kInitialFilters;
+
 
   void _selectedPage(int index) {
     setState(() {
@@ -39,38 +39,34 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
     if (indetifer == 'filters') {
       Navigator.of(context).pop();
       //pushReplacement lecserélödik a képernyő emulátoron se müködik a vissza gomb
-      final result = await Navigator.of(context).push<Map<Filter, bool>>(
+      await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
-          builder: (ctx) => FiltersScreen(
-            currentFilters: _selectedFilters,
-          ),
+          builder: (ctx) => const FiltersScreen(),
         ),
-      );
-
-      setState(() {
-        _selectedFilters = result ?? kInitialFilters;
-      });
+      );     
     }
   }
 
   @override
   Widget build(BuildContext context) {
     var activePageTitle = 'Categories';
-//meals provider meghívása 
+//meals provider meghívása
     final meals = ref.watch(mealsProvider);
+    final activeFilters = ref.watch(filtersProvider);
     //dummyMeals szűrése a feltételek alapján
-    final availableMeals = meals.where(/// dummyMeals helyett már a providerben lévő meals-t használja
+    final availableMeals = meals.where(
+      /// dummyMeals helyett már a providerben lévő meals-t használja
       (meal) {
-        if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
           return false;
         }
-        if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
           return false;
         }
-        if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
           return false;
         }
-        if (_selectedFilters[Filter.vegan]! && !meal.isVegetarian) {
+        if (activeFilters[Filter.vegan]! && !meal.isVegetarian) {
           return false;
         }
         return true;
@@ -84,8 +80,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       final favotiteMeals = ref.watch(favoritesMealsProvider);
-      activePage = MealsScreen(
-          meals: favotiteMeals);
+      activePage = MealsScreen(meals: favotiteMeals);
       activePageTitle = 'Your Favorites';
     }
 
