3f82a0e7988a9da76a128af893b737bc5eb277f2
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-31 09:34:29 +0200

    starting project

---
+++ animacion_meals_app/lib/screens/categories.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key,required this.availableMeals});  
+
+    final List<Meal> availableMeals;
+
+
+  
+  void _selectCategory(BuildContext context, Category category) {
+
+    
+    final filteredMeals = availableMeals 
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+      
+        .toList();
+
+    //Navigáció
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        //MealsScreen-re navigál át
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,        
+        ),
+      ),
+    ); // Navigator.push(context, route)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  GridView(
+        padding: const EdgeInsets.all(16),      
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              //függvény átadás
+              onSelectedCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+
+          //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+        ],
+      );
+    
+  }
+}
---
+++ animacion_meals_app/lib/screens/tabs.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/providers/favorites_provider.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/filter_provider.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false
+};
+class TabsScreen extends ConsumerStatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends ConsumerState<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  void _setScreen(String indetifer) async {    
+    if (indetifer == 'filters') {
+      Navigator.of(context).pop();
+      await Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
+      );     
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   final availableMeals =ref.watch(filteredMealsProvider);
+    
+   
+
+    Widget activePage = CategoriesScreen(
+      availableMeals: availableMeals,
+    );
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      final favoriteMeals = ref.watch(favoritesMealsProvider);
+      activePage = MealsScreen(
+        meals: favoriteMeals,
+      );
+      activePageTitle = 'Your Favorites';
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectedPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
+        ],
+      ),
+    );
+  }
+}
