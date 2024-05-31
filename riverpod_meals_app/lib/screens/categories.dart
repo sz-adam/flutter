2744379929cddcf8004da4bb265e183721a3d907
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-05-31 10:04:20 +0200

    gridView animation

--- riverpod_meals_app/lib/screens/categories.dart
+++ riverpod_meals_app/lib/screens/categories.dart
@@ -5,23 +5,47 @@ import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key,required this.availableMeals});  
+//az animációkhoz StatefulWidget-re van szükség
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({super.key, required this.availableMeals});
 
-    final List<Meal> availableMeals;
+  final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override //animáció meghívása
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 300),
+        lowerBound: 0,
+        upperBound: 1);
+    _animationController.forward();
+  }
+
+  @override //ha lefutott az animáció törlődik a memóriából
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   //NAVIGÁCIÓ
-  //context nem érhető el ezért kell meghívni
   void _selectCategory(BuildContext context, Category category) {
-
     //dummyMeals listát szűri olyan elemekre, amelyek tartalmazzák az adott kategória azonosítóját
-    final filteredMeals = availableMeals //használata a szűréshez
-    //where csak azokat az elemeket tartalmazza, amelyek megfelelnek a megadott feltételnek.
+    final filteredMeals = widget.availableMeals //használata a szűréshez
+        //where csak azokat az elemeket tartalmazza, amelyek megfelelnek a megadott feltételnek.
         .where(
           (meal) => meal.categories.contains(category.id),
         )
-        //listává alakítjuk 
+        //listává alakítjuk
         .toList();
 
     //Navigáció
@@ -30,7 +54,7 @@ class CategoriesScreen extends StatelessWidget {
         //MealsScreen-re navigál át
         builder: (ctx) => MealsScreen(
           title: category.title,
-          meals: filteredMeals,        
+          meals: filteredMeals,
         ),
       ),
     ); // Navigator.push(context, route)
@@ -38,29 +62,35 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  GridView(
-        padding: const EdgeInsets.all(16),
-        //grid elem :crossAxisCount 2 oszlop ,childAspectRatio: 3 / 2, // Szélesség / Magasság arány
-        //rács elemek közötti távolság crossAxisSpacing: 20, mainAxisSpacing: 20
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children: [
-          //végig megyünk a availableCategories gyüjteményen és minden egyes elemhez létrehoz egy CategoryGridItem widgetet
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              //függvény átadás
-              onSelectedCategory: () {
-                _selectCategory(context, category);
-              },
-            )
+    return AnimatedBuilder(
+        animation: _animationController,
+        child: GridView(
+          padding: const EdgeInsets.all(16),
+          //grid elem :crossAxisCount 2 oszlop ,childAspectRatio: 3 / 2, // Szélesség / Magasság arány
+          //rács elemek közötti távolság crossAxisSpacing: 20, mainAxisSpacing: 20
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: [
+            //végig megyünk a availableCategories gyüjteményen és minden egyes elemhez létrehoz egy CategoryGridItem widgetet
+            for (final category in availableCategories)
+              CategoryGridItem(
+                category: category,
+                //függvény átadás
+                onSelectedCategory: () {
+                  _selectCategory(context, category);
+                },
+              )
 
-          //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-        ],
-      );
-    
+            //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+          ],
+        ),
+        builder: (context, child) => Padding(
+              padding:
+                  EdgeInsets.only(top: 200 - _animationController.value * 200),
+              child: child,
+            ));
   }
 }
