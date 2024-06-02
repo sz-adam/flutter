069cfd0b1fd9265fcad548d6ab2a48eaff4e982b
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-02 17:15:46 +0200

    add reset and add button

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -18,59 +18,68 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
-            child: Column(
-          children: [
-            //form specifikus widget Név
-            TextFormField(
-              maxLength: 50,
-              decoration:const InputDecoration(
-                label: Text('Name'),
+          child: Column(
+            children: [
+              //form specifikus widget Név
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                //lehetővé teszi egy fügvény hozzáadását
+                validator: (value) {
+                  return 'demo...';
+                },
               ),
-              //lehetővé teszi egy fügvény hozzáadását
-              validator: (value) {
-                return 'demo...';
-              },
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Quantity'),
-                    ),
-                    //kezdeti érték , stringként kell kezelni
-                    initialValue: '1',
-                  ),
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                Expanded(
-                  child: DropdownButtonFormField(items: [
-                    for (final category in categories.entries)
-                      DropdownMenuItem(
-                        value: category.value,
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 16,
-                              height: 16,
-                              color: category.value.color,
-                            ),
-                            const SizedBox(
-                              width: 6,
-                            ),
-                            Text(category.value.title),
-                          ],
-                        ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
                       ),
-                  ], onChanged: (value){}),
-                )
-              ],
-            )
-          ],
-        )),
+                      //kezdeti érték , stringként kell kezelni
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ], onChanged: (value) {}),
+                  )
+                ],
+              ),
+              const SizedBox(height: 12,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(onPressed: (){}, child: const Text('Reset'),),
+                  ElevatedButton(onPressed: (){}, child:const Text('Add item'))
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
