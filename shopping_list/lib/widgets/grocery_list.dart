dde06e5248852719da06550e31b11d86d524b320
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-02 18:55:40 +0200

    delete function

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -27,6 +27,12 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item){
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
         Widget content = const Center(
@@ -36,16 +42,22 @@ class _GroceryListState extends State<GroceryList> {
     if(_groceryItems.isNotEmpty) {
       content =ListView.builder(
         itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(
-            _groceryItems[index].name,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction){
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(
+              _groceryItems[index].name,
+            ),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
           ),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(_groceryItems[index].quantity.toString()),
         ),
       );
     }
