d325014939933557a47d5d7db92bd2fbaf7f7a07
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-08 15:54:39 +0200

    add place Screen

--- favorite_places_app/lib/screens/places.dart
+++ favorite_places_app/lib/screens/places.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places_app/screens/add_place.dart';
 import 'package:favorite_places_app/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,12 @@ class PlacesScreen extends StatelessWidget {
         title: const Text('Your Places'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
             icon: const Icon(Icons.add),
           ),
         ],
