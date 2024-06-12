aad48ccddfe66eed741c92da23d9e39fa2607432
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-12 10:14:31 +0200

    Storing Place Data in a (On-Device) SQL Database

--- favorite_places_app/lib/providers/user_places.dart
+++ favorite_places_app/lib/providers/user_places.dart
@@ -1,12 +1,47 @@
 import 'package:favorite_places_app/models/place.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // Riverpod állapotkezelő csomag importálása
+import 'dart:io'; // Fájlkezeléshez szükséges csomag
+import 'package:path_provider/path_provider.dart' as syspaths; // Helyi fájlútvonalak eléréséhez szükséges csomag
+import 'package:path/path.dart' as path; // Útvonalak kezeléséhez szükséges csomag
+import 'package:sqflite/sqflite.dart' as sql; // SQLite adatbázis kezelő csomag
+import 'package:sqflite/sqlite_api.dart'; // SQLite API csomag
 
 class UserPlacesNotifier extends StateNotifier<List<Places>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Places(title: title,image: image,location: location);
+
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory(); // Az alkalmazás dokumentum könyvtárának elérése
+    final filename = path.basename(image.path); // Az eredeti kép fájlnevének kinyerése
+    final copiedImage = await image.copy('${appDir.path}/$filename'); // Kép másolása az alkalmazás dokumentum könyvtárába
+
+    final newPlace =
+        Places(title: title, image: copiedImage, location: location);
+
+    // Adatbázis útvonalának lekérése
+    final dbPath = await sql.getDatabasesPath();
+    // Adatbázis megnyitása vagy létrehozása, ha nem létezik
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        // Táblázat létrehozása, ha az adatbázis létrehozásra kerül
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, city TEXT)');
+      },
+      version: 1,
+    );
+
+    // Új hely adatok beszúrása az adatbázisba
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longItude,
+      'city': newPlace.location.city
+    });
+
+    // Az állapot frissítése az új hely hozzáadásával
     state = [newPlace, ...state];
   }
 }
