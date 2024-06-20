787681481b43f128559469f492056845cf424a74
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-20 19:00:05 +0200

    Loading & Displaying Chat Messages as a Stream

--- flutter_chat/lib/widgets/chat_messages.dart
+++ flutter_chat/lib/widgets/chat_messages.dart
@@ -1,10 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
-const ChatMessages({ Key? key }) : super(key: key);
+  const ChatMessages({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return const Center(child: Text('No messages found'),);
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(), // Firestore gyűjtemény lekérése és rendezése a 'createdAt' mező alapján növekvő sorrendben, folyamatos stream figyelése
+      builder: (ctx, chatSnapshot) {
+        // A StreamBuilder által figyelt adatok alapján építjük fel a widgetet
+
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          // Ha a kapcsolat állapota várakozás
+          return const Center(
+            child: CircularProgressIndicator(), // Mutatja a betöltési jelzőt
+          );
+        }
+
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          // Ha nincs adat vagy az adatok üresek
+          return const Center(
+            child: Text('No messages found'), // Üzenet megjelenítése, ha nincsenek üzenetek
+          );
+        }
+
+        if (chatSnapshot.hasError) {
+          // Ha hiba történik az adatlekérés során
+          return const Center(
+            child: Text('Something went wrong...'), // Hibajelzés megjelenítése
+          );
+        }
+
+        final loadedMessages = chatSnapshot.data!.docs; // Az üzenetek lekérése az adatokból
+
+        return ListView.builder(
+          itemCount: loadedMessages.length, // Az üzenetek számának megadása a ListView számára
+          itemBuilder: (ctx, index) =>
+              Text(loadedMessages[index].data()['text']), // Az üzenetek szövegének megjelenítése
+        );
+      },
+    );
   }
 }
